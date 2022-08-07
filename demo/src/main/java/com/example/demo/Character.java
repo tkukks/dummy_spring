ba4e754c110546c06b69ea7f5caa133d1f859085
Tyler Kukkola <tyler.kukkola@gmail.com>
2022-08-06 23:37:00 -0700

    initial commit

---
+++ demo/src/main/java/com/example/demo/Character.java
@@ -0,0 +1,58 @@
+package com.example.demo;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class Character {
+    private String name;
+
+    @JsonProperty("hair_color")
+    private String hairColor;
+
+    @JsonProperty("skin_color")
+    private String skinColor;
+
+    @JsonProperty("eye_color")
+    private String eyeColor;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getHairColor() {
+        return hairColor;
+    }
+
+    public void setHairColor(String hairColor) {
+        this.hairColor = hairColor;
+    }
+
+    public String getSkinColor() {
+        return skinColor;
+    }
+
+    public void setSkinColor(String skinColor) {
+        this.skinColor = skinColor;
+    }
+
+    public String getEyeColor() {
+        return eyeColor;
+    }
+
+    public void setEyeColor(String eyeColor) {
+        this.eyeColor = eyeColor;
+    }
+
+    @Override
+    public String toString() {
+        return "Character{" +
+                "name='" + name + '\'' +
+                ", hairColor='" + hairColor + '\'' +
+                ", skinColor='" + skinColor + '\'' +
+                ", eyeColor='" + eyeColor + '\'' +
+                '}';
+    }
+}
